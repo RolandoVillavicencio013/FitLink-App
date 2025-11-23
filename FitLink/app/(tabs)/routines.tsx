647851544c717d4df7fe22605ebb7d55196b68f7
Brayan Rivera <Brayanrive004@gmail.com>
2025-11-22 20:30:41 -0600

    Se agrega la pantalla para ver las rutinas del usuario

--- FitLink/app/(tabs)/routines.tsx
+++ FitLink/app/(tabs)/routines.tsx
@@ -1,25 +1,110 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { supabase } from '../../services/supabase';
+import { theme } from '../../constants/theme';
 
-const Routines: React.FC = () => {
+interface routine{
+  routine_id: number;
+  name: string;
+  description: string;
+  is_shared: boolean;
+  created_at: string;
+  user_id: string;
+  estimated_time: number;
+  routine_exercises: [];
+}
+
+export default function RoutinesScreen() {
+  const [routines, setRoutines] = useState<routine[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadRoutines();
+  }, []);
+
+  async function loadRoutines() {
+    const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+    if (userError || !user) {
+      console.error(userError);
+      return;
+    }
+
+    const { data: users, error: usersError } = await supabase
+      .from("users")
+      .select("user_id")
+      .eq("auth_id", user.id)
+      .single();
+
+    if (usersError || !users) {
+      console.error(userError);
+      return;
+    }
+
+    const { data, error } = await supabase
+    .from('routines')
+    .select(`
+      routine_id,
+      name,
+      description,
+      is_shared,
+      created_at,
+      user_id,
+      estimated_time,
+      routine_exercises (
+        exercise_id
+      )
+    `)
+    .eq('user_id', users.user_id);
+
+    if (error) {
+      console.error(error);
+    } else {
+      setRoutines(data);
+    }
+
+    setLoading(false);
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Estas son las rutinas
-    
-      </Text>
+      <Text style={styles.message} >Haz click en una rutina para ver mas detalles</Text>
+      <FlatList
+        data={routines}
+        keyExtractor={(item) => item.routine_id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.title}>{item.name}</Text>
+            <Text style={styles.exercises}>{'Cantidad de ejercicios: ' + (item.routine_exercises?.length ?? 0)}</Text>
+            <Text style={styles.time}>{'Tiempo estimado: ' + item.estimated_time + ' minutos'}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-};
-
-export default Routines;
+}
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center'
+  card: {
+    backgroundColor: theme.colors.surface,
+    borderColor: theme.colors.border,
+    borderRadius: 10,
+    borderWidth: 2,                    
+    marginBottom: 10,
+    padding: 15,
   },
-  text: {
-    fontSize: 20
-  }
+  center: { alignItems: 'center', flex: 1, justifyContent: 'center' },
+  container: { padding: 20 },
+  exercises: {color: theme.colors.textSecondary, fontFamily: "Roboto_400Regular", fontSize: 16, marginBottom: 4},
+  message: {  color: theme.colors.textPrimary, fontFamily: "Roboto_400Regular", fontSize: 17.5, marginBottom: 20,},
+  time: {color: theme.colors.textSecondary, fontFamily: "Roboto_400Regular", fontSize: 16, },
+  title: { color: theme.colors.textPrimary, fontFamily: "Roboto_400Regular", fontSize: 16, fontWeight: 'bold', marginBottom: 8},
 });
--- FitLink/constants/theme.ts
+++ FitLink/constants/theme.ts
@@ -8,6 +8,7 @@ export const theme = {
     textSecondary: "#B3B3B3",  // texto secundario
     success: "#4CAF50",        // verde para éxito
     error: "#F44336",          // rojo para error
-    divider: "#2A2A2A"         // gris intermedio para separadores
+    divider: "#2A2A2A",         // gris intermedio para separadores
+    border: "#b593fdff",
   }
 };
