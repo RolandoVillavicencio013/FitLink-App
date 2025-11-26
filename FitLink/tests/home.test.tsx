514b25fb7231340e5e31a40062e0c38a591d1fd3
Rolando Villavicencio <jose.villavicenciogonzalez@ucr.ac.cr>
2025-11-25 19:43:33 -0600

    Reorganiza código en carpeta src/ y configura SonarCloud con CI/CD

--- FitLink/tests/home.test.tsx
+++ FitLink/tests/home.test.tsx
@@ -1,7 +1,7 @@
 // tests/home.test.tsx
 import { render } from '@testing-library/react-native';
 import React from 'react';
-import Home from '../app/(tabs)/home';
+import Home from '../src/app/(tabs)/home';
 
 describe('Home screen', () => {
   it('renders correctly', () => {
