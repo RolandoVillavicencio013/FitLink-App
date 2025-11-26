6e63e45daf45734a907a1a51b999f9cdc89d268e
Rolando Villavicencio <jose.villavicenciogonzalez@ucr.ac.cr>
2025-11-25 20:32:43 -0600

    Corrige configuración de Jest eliminando dependencia deprecada

--- FitLink/jest.config.js
+++ FitLink/jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
     'node_modules/(?!(expo-modules-core|expo|react-native|@react-native|@expo|@unimodules)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testEnvironment: 'jsdom',
 };
